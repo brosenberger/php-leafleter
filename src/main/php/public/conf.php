a8e986ec3eeed8ce649ffda9f36a6ff20dc9c719
Benjamin Rosenberger <bensch.rosenberger@gmail.com>
2016-11-15 05:49:20 +0100

    [FIX] fix autoloading for linux based servers

--- src/main/php/public/conf.php
+++ src/main/php/public/conf.php
@@ -1,7 +1,7 @@
 <?php
 require '../vendor/autoload.php';
 spl_autoload_register(function ($classname) {
-    require ("../classes/" . $classname . ".php");
+    require (realpath(dirname(__FILE__)) . "/../classes/" . str_replace("\\", "/", $classname) . ".php");
 }, false);
 
 $config = array();
