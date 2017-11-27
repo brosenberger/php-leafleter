4730199f3d9a210c3fec2406bde41727cd0c62b4
Benjamin Rosenberger <bensch.rosenberger@gmail.com>
2017-11-27 23:11:36 +0100

    :sparkle: add marker clustering to map feature

--- src/main/php/resources/js/v1/php-leafleter-js.js
+++ src/main/php/resources/js/v1/php-leafleter-js.js
@@ -12,6 +12,9 @@
 				
 				var iconOptionGenerator = function(element, options) {
 					var iconOptions = options || {};
+					if (!element) {
+						return iconOptions;
+                    }
 					if (element['size']) {
 						iconOptions['iconSize'] = element['size'];
 					}
@@ -25,7 +28,7 @@
 					return iconOptions;
 				};
 				var iconUrlGenerator = function(token, url) {
-					return '/images/'+token+'/'+url;
+					return url;
 				};
 				
 				// load datapoints
@@ -33,10 +36,11 @@
 				    options: iconOptionGenerator(configuration['icon'][0])
 				});
 				var icon = new MapIcon({iconUrl:iconUrlGenerator(config['token'],configuration['icon'][0]['url'])});
+				var markerCluster = L.markerClusterGroup();
 				$.getJSON(config['url']+'points?token='+config['token']).done(function(data) {
 					$.each(data, function(index, element) {
 					
-					 	var markerConfiguration iconOptionGenerator(element['img'], ;
+					 	var markerConfiguration = iconOptionGenerator(element['img']) ;
 						if (element['imgUrl']) {
 							markerConfiguration = {
 								icon: new MapIcon({iconUrl:'/images/'+config['token']+'/'+element['imgUrl']})
@@ -44,15 +48,20 @@
 						} else {
 							markerConfiguration = {icon: icon}
 						}
-						var marker = L.marker([element['lat'], element['lng']],  markerConfiguration).addTo(mymap);
+						var marker = L.marker([element['lat'], element['lng']],  markerConfiguration);
 						if (config['callback']) {
 							config['callback'](marker, element);
 						}
+						markerCluster.addLayer(marker);
 					});
 				});
+				mymap.addLayer(markerCluster);
 				
 				// load features
 				$.getJSON(config['url']+'features?token='+config['token']).done(function(region) {
+					if ($.isEmptyObject(region)) {
+						return;
+					}
 					L.geoJson(region, {style: function(feature) {
 						return {
 							fillColor: configuration['region'][0]['fillColor'],
