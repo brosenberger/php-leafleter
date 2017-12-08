a01dbfdff658d378f0e07c60156abe2275f06474
Benjamin Rosenberger <bensch.rosenberger@gmail.com>
2017-12-08 10:21:35 +0100

    :sparkles: add trailing slash for map routes

--- src/main/php/classes/Controller/MapRoutes.php
+++ src/main/php/classes/Controller/MapRoutes.php
@@ -11,8 +11,9 @@ class MapRoutes implements IRouter
             return $this->view->render($response, 'map.html', $that->_prepareMapConfiguration($this->resourceService, $this->settings['default_token']));
         })
             ->setName('map');
-        $app->get('/t/{token}', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args) use ($that) {
-            return $this->view->render($response, 'map.html', $that->_prepareMapConfiguration($this->resourceService, $request->getAttribute('token')));
+
+        $app->get('/t/{token}[/]', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args) use ($that) {
+            return $this->view->render($response, 'map.html', $that->_prepareMapConfiguration($this->resourceService, $args['token']));
         })
             ->setName('tmap');
     }
